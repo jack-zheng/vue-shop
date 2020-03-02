aecebf7847d66532fce7a8a3eaeedc41b777dd21
jack-zheng <lanmolei0124@163.com>
2020-03-03 00:52:54 +0800

    mean layout

--- vue_shop/src/components/Home.vue
+++ vue_shop/src/components/Home.vue
@@ -2,13 +2,42 @@
   <el-container class="home-container">
     <el-header>
       <div>
-        <img src="../assets/heima.png" alt="">
-        <span> 电商后台管理系统 </span>
+        <img src="../assets/heima.png" alt />
+        <span>电商后台管理系统</span>
       </div>
       <el-button type="info" @click="logout">退出</el-button>
     </el-header>
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <!-- 侧边栏区域 -->
+      <el-aside width="200px">
+        <!-- 侧边栏菜单 -->
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+        <!-- 一级菜单 -->
+          <el-submenu index="1">
+            <!-- 一级菜单模板区 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i class="el-icon-location"></i>
+              <!-- 文本 -->
+              <span>导航一</span>
+            </template>
+
+            <!-- 二级菜单 -->
+            <el-menu-item index="1-4-1">
+              <template slot="title">
+              <!-- 图标 -->
+              <i class="el-icon-location"></i>
+              <!-- 文本 -->
+              <span>导航一</span>
+            </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-main>Main</el-main>
     </el-container>
   </el-container>
@@ -31,20 +60,20 @@ export default {
 }
 
 .el-header {
- background-color: #373d41;
- display: flex;
- justify-content: space-between;
- padding-left: 0;
- align-items: center;
- color: #fff;
- font-size: 20px;
- > div {
-   display: flex;
-   align-items: center;
-   span {
-     margin-left: 15px;
-   }
- }
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
 }
 
 .el-aside {
