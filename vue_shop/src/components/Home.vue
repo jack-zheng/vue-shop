163b28f07634fd2a65c0d354df9d2bd2a41da22f
jack-zheng <lanmolei0124@163.com>
2020-03-03 01:16:20 +0800

    show aside menu

--- vue_shop/src/components/Home.vue
+++ vue_shop/src/components/Home.vue
@@ -17,22 +17,22 @@
           active-text-color="#ffd04b"
         >
         <!-- 一级菜单 -->
-          <el-submenu index="1">
+          <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
             <!-- 一级菜单模板区 -->
             <template slot="title">
               <!-- 图标 -->
               <i class="el-icon-location"></i>
               <!-- 文本 -->
-              <span>导航一</span>
+              <span>{{ item.authName }}</span>
             </template>
 
             <!-- 二级菜单 -->
-            <el-menu-item index="1-4-1">
+            <el-menu-item index="1-4-1" v-for="subItem in item.children" :key="subItem.id">
               <template slot="title">
               <!-- 图标 -->
               <i class="el-icon-location"></i>
               <!-- 文本 -->
-              <span>导航一</span>
+              <span>{{ subItem.authName }}</span>
             </template>
             </el-menu-item>
           </el-submenu>
