f71a22c640c346a2a862c989dc07b1041bdd9247
jack-zheng <lanmolei0124@163.com>
2020-03-04 00:35:00 +0800

    center style for roles

--- vue_shop/src/components/power/Roles.vue
+++ vue_shop/src/components/power/Roles.vue
@@ -20,7 +20,7 @@
       <el-table :data="rolelist" border stripe>
           <el-table-column type="expand">
               <template slot-scope="scope">
-                  <el-row :class="['bdbottom', i1 === 0 ? 'bdtop' : '']" v-for="(item1, i1) in scope.row.children" :key="item1.id">
+                  <el-row :class="['bdbottom', i1 === 0 ? 'bdtop' : '', 'vcenter']" v-for="(item1, i1) in scope.row.children" :key="item1.id">
                       <!-- 渲染一级权限 -->
                       <el-col :span="5">
                           <el-tag> {{ item1.authName }} </el-tag>
@@ -29,7 +29,7 @@
                       <!-- 渲染二级权限 -->
                       <el-col :span="19">
                           <!-- 通过 for 嵌套渲染二级权限 -->
-                          <el-row :class="[i2 === 0? '' : 'bdtop']" v-for="(item2, i2) in item1.children" :key="item2.id">
+                          <el-row :class="[i2 === 0? '' : 'bdtop', 'vcenter']" v-for="(item2, i2) in item1.children" :key="item2.id">
                               <el-col :span="6">
                                 <el-tag type="success"> {{ item2.authName }} </el-tag>
                                 <i class="el-icon-caret-right"></i>
@@ -40,9 +40,6 @@
                           </el-row>
                       </el-col>
                   </el-row>
-                  <pre>
-                    {{ scope.row }}
-                  </pre>
               </template>
           </el-table-column>
           <!-- 索引列 -->
@@ -98,5 +95,10 @@ export default {
 
 .bdbottom {
     border-bottom: 1px solid #eee;
+}
+
+.vcenter {
+    display: flex;
+    align-items: center;
 }
 </style>
