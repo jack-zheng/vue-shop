4ad4c6842e23671462f0d98c21228905e7e64b7c
jack-zheng <lanmolei0124@163.com>
2020-03-03 23:57:49 +0800

    layout of roles table

--- vue_shop/src/components/power/Roles.vue
+++ vue_shop/src/components/power/Roles.vue
@@ -15,7 +15,22 @@
           <el-button type="primary">添加角色</el-button>
         </el-col>
       </el-row>
+
       <!-- 角色列表 -->
+      <el-table :data="rolelist" border stripe>
+          <el-table-column type="expand"></el-table-column>
+          <!-- 索引列 -->
+          <el-table-column type="index"></el-table-column>
+          <el-table-column label="角色名称" prop="roleName"></el-table-column>
+          <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
+          <el-table-column label="操作" width="300px">
+              <template>
+                <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+                <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+                <el-button type="warning" icon="el-icon-setting" size="mini">分配权限</el-button>
+              </template>
+          </el-table-column>
+      </el-table>
     </el-card>
   </div>
 </template>
