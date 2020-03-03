8a8e64bb3480a4b74b5b913cba9e7f395965baa4
jack-zheng <lanmolei0124@163.com>
2020-03-03 18:34:57 +0800

    search field layout

--- vue_shop/src/plugins/element.js
+++ vue_shop/src/plugins/element.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import {
   Button, Form, FormItem, Input,
   Message, Container, Header, Aside,
-  Main, Menu, Submenu, MenuItem
+  Main, Menu, Submenu, MenuItem,
+  Breadcrumb, BreadcrumbItem, Card,
+  Row, Col
 } from 'element-ui'
 
 Vue.use(Button)
@@ -16,4 +18,9 @@ Vue.use(Main)
 Vue.use(Menu)
 Vue.use(Submenu)
 Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
 Vue.prototype.$message = Message
