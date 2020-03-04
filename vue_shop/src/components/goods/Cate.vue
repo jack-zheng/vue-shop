1579d31fae6361154237d12f417f0ae7d01cd75d
jack-zheng <lanmolei0124@163.com>
2020-03-04 20:44:39 +0800

    basic structure of page

--- vue_shop/src/components/goods/Cate.vue
+++ vue_shop/src/components/goods/Cate.vue
@@ -1,6 +1,27 @@
 <template>
   <div>
-    <h3>Goods</h3>
+    <!-- 面包屑导航区 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图 -->
+    <el-card class="box-card">
+      <!-- 添加角色按钮区 -->
+      <el-row>
+        <el-col>
+          <el-button type="primary">添加分类</el-button>
+        </el-col>
+      </el-row>
+
+      <!-- 表格区 -->
+      <el-table :data="rolelist" border stripe>
+        <el-table-column type="expand"></el-table-column>
+      </el-table>
+      <!-- 分页区 -->
+    </el-card>
   </div>
 </template>
 
