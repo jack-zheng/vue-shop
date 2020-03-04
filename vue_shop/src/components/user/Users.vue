b8e901925b65a5c5660f08e54df4c0b3d25d1855
jack-zheng <lanmolei0124@163.com>
2020-03-04 19:29:05 +0800

    add selection to dialog

--- vue_shop/src/components/user/Users.vue
+++ vue_shop/src/components/user/Users.vue
@@ -120,6 +120,17 @@
     <div>
       <p> 当前的用户： {{ userInfo.username }} </p>
       <p> 当前的角色： {{ userInfo.role_name }} </p>
+      <p>
+        分配新角色：
+        <el-select v-model="selectedRoleId" placeholder="请选择">
+          <el-option
+            v-for="item in rolesList"
+            :key="item.id"
+            :label="item.roleName"
+            :value="item.id">
+          </el-option>
+        </el-select>
+      </p>
     </div>
     <span slot="footer" class="dialog-footer">
       <el-button @click="setRoleDialogVisible = false">取 消</el-button>
@@ -213,7 +224,9 @@ export default {
       // 需要被分配角色的用户信息
       userInfo: {},
       // 所有角色的数据列表
-      rolesList: []
+      rolesList: [],
+      // 已选中的 role id 值
+      selectedRoleId: ''
     }
   },
   created() {
--- vue_shop/src/plugins/element.js
+++ vue_shop/src/plugins/element.js
@@ -6,7 +6,7 @@ import {
   Breadcrumb, BreadcrumbItem, Card,
   Row, Col, Table, TableColumn,
   Switch, Tooltip, Pagination, Dialog,
-  MessageBox, Tag, Tree
+  MessageBox, Tag, Tree, Select, Option
 } from 'element-ui'
 
 Vue.use(Button)
@@ -33,5 +33,7 @@ Vue.use(Pagination)
 Vue.use(Dialog)
 Vue.use(Tag)
 Vue.use(Tree)
+Vue.use(Select)
+Vue.use(Option)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
