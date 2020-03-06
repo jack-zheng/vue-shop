1a31191bfc08937c34375b32ce5782a686abc877
jack-zheng <lanmolei0124@163.com>
2020-03-06 20:51:09 +0800

    tag work with steps bar

--- vue_shop/src/components/goods/Add.vue
+++ vue_shop/src/components/goods/Add.vue
@@ -11,7 +11,7 @@
       <!-- 提示区 -->
       <el-alert title="添加商品信息" type="info" center show-icon :closable="false"></el-alert>
       <!-- 步骤条 -->
-      <el-steps :space="200" :active="activeIndex" finish-status="success" align-center>
+      <el-steps :space="200" :active="activeIndex - 0" finish-status="success" align-center>
         <el-step title="基本信息"></el-step>
         <el-step title="商品参数"></el-step>
         <el-step title="商品属性"></el-step>
@@ -21,12 +21,12 @@
       </el-steps>
 
       <!-- tab栏区域 -->
-      <el-tabs :tab-position="'left'" style="height: 200px;">
-        <el-tab-pane label="基本信息">基本信息</el-tab-pane>
-        <el-tab-pane label="商品参数">商品参数</el-tab-pane>
-        <el-tab-pane label="商品属性">商品属性</el-tab-pane>
-        <el-tab-pane label="商品图片">商品图片</el-tab-pane>
-        <el-tab-pane label="商品内容">商品内容</el-tab-pane>
+      <el-tabs :tab-position="'left'" style="height: 200px;" v-model="activeIndex">
+        <el-tab-pane label="基本信息" name="0">基本信息</el-tab-pane>
+        <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
+        <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
+        <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
+        <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
       </el-tabs>
     </el-card>
   </div>
@@ -36,7 +36,7 @@
 export default {
   data() {
     return {
-      activeIndex: 0
+      activeIndex: '0'
     }
   },
   created() {},
