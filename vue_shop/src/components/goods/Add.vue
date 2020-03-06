651afdf95bc8222866f491fd3d8815444663554b
jack-zheng <lanmolei0124@163.com>
2020-03-06 20:31:32 +0800

    new router for add page

---
+++ vue_shop/src/components/goods/Add.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>add goods</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  created() {},
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+</style>
--- vue_shop/src/components/goods/List.vue
+++ vue_shop/src/components/goods/List.vue
@@ -16,7 +16,7 @@
           </el-input>
         </el-col>
         <el-col :span="4">
-          <el-button type="primary">添加商品</el-button>
+          <el-button type="primary" @click="goAddPage">添加商品</el-button>
         </el-col>
       </el-row>
 
@@ -120,6 +120,9 @@ export default {
       }
       this.getGoodsList()
       this.$message.success('商品已删除')
+    },
+    goAddPage() {
+      this.$router.push('/goods/add')
     }
   }
 }
