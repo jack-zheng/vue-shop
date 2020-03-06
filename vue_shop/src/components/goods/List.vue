d46e3fdedf37412f5332ab760ded3f46d06de706
jack-zheng <lanmolei0124@163.com>
2020-03-06 19:27:12 +0800

    add time filter

--- vue_shop/src/components/goods/List.vue
+++ vue_shop/src/components/goods/List.vue
@@ -26,7 +26,9 @@
         <el-table-column label="商品名称" prop="goods_name"></el-table-column>
         <el-table-column label="商品价格（元）" prop="goods_price" width="90px"></el-table-column>
         <el-table-column label="商品重量" prop="goods_weight" width="70px"></el-table-column>
-        <el-table-column label="商品创建时间" prop="add_time" width="140px"></el-table-column>
+        <el-table-column label="商品创建时间" prop="add_time" width="170px">
+          <template slot-scope="scope">{{ scope.row.add_time | dateFormat}}</template>
+        </el-table-column>
         <el-table-column label="操作" width="140px">
           <template slot-scope>
             <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
