9610e6eab3cab0e0f5e143eeafe5052ef54de490
jack-zheng <lanmolei0124@163.com>
2020-03-07 17:16:39 +0800

    render chart

--- vue_shop/src/components/report/Report.vue
+++ vue_shop/src/components/report/Report.vue
@@ -9,46 +9,66 @@
 
     <!-- 卡片视图 -->
     <el-card class="box-card">
-      <div id="main" style="width: 600px;height:400px;"></div>
+      <div id="main" style="width: 750px;height:400px;"></div>
     </el-card>
   </div>
 </template>
 
 <script>
 import echarts from 'echarts'
-
+import _ from 'lodash'
 export default {
   data() {
-    return {}
+    return {
+      // 需要合并的数据
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    }
   },
   craeted() {},
   // 此时页面上的元素已经被渲染完毕了
-  mounted() {
+  async mounted() {
     // 基于准备好的dom，初始化echarts实例
     var myChart = echarts.init(document.getElementById('main'))
-    var option = {
-      title: {
-        text: 'ECharts 入门示例'
-      },
-      tooltip: {},
-      legend: {
-        data: ['销量']
-      },
-      xAxis: {
-        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-      },
-      yAxis: {},
-      series: [
-        {
-          name: '销量',
-          type: 'bar',
-          data: [5, 20, 36, 10, 10, 20]
-        }
-      ]
+
+    const { data: res } = await this.$http.get('reports/type/1')
+    if (res.meta.status !== 200) {
+      return this.$message.error('折线图数据获取失败')
     }
 
+    // 准备数据和配置项
+    const result = _.merge(res.data, this.options)
+
     // 展示数据
-    myChart.setOption(option)
+    myChart.setOption(result)
   },
   methods: {}
 }
