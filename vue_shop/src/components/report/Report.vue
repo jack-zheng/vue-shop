39bab6870c90186ca348d00a91083056da433b6b
jack-zheng <lanmolei0124@163.com>
2020-03-07 17:05:45 +0800

    setup echarts demo

--- vue_shop/src/components/report/Report.vue
+++ vue_shop/src/components/report/Report.vue
@@ -8,16 +8,48 @@
     </el-breadcrumb>
 
     <!-- 卡片视图 -->
-    <el-card class="box-card">123</el-card>
+    <el-card class="box-card">
+      <div id="main" style="width: 600px;height:400px;"></div>
+    </el-card>
   </div>
 </template>
 
 <script>
+import echarts from 'echarts'
+
 export default {
   data() {
     return {}
   },
   craeted() {},
+  // 此时页面上的元素已经被渲染完毕了
+  mounted() {
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('main'))
+    var option = {
+      title: {
+        text: 'ECharts 入门示例'
+      },
+      tooltip: {},
+      legend: {
+        data: ['销量']
+      },
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      },
+      yAxis: {},
+      series: [
+        {
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }
+      ]
+    }
+
+    // 展示数据
+    myChart.setOption(option)
+  },
   methods: {}
 }
 </script>
